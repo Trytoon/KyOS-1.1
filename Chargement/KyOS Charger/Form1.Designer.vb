80cd14f0cbc61b57e3e81655f772c5462dcc6776
KyneasSurYoutube <31820002+KyneasSurYoutube@users.noreply.github.com>
2018-10-14 17:56:06 +0200

    Add files via upload

---
+++ Chargement/KyOS Charger/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Logo = New System.Windows.Forms.PictureBox()
+        Me.Bar = New System.Windows.Forms.ProgressBar()
+        Me.TextChargement = New System.Windows.Forms.Timer(Me.components)
+        Me.Chargement = New System.Windows.Forms.Timer(Me.components)
+        Me.ChargementText = New KyOS_Charger.FlatLabel()
+        Me.Copyright = New KyOS_Charger.FlatLabel()
+        Me.MAJtimer = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.Logo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Logo
+        '
+        Me.Logo.Image = Global.KyOS_Charger.My.Resources.Resources.kyos_icon2
+        Me.Logo.InitialImage = Global.KyOS_Charger.My.Resources.Resources.kyos_icon2
+        Me.Logo.Location = New System.Drawing.Point(10, 7)
+        Me.Logo.Name = "Logo"
+        Me.Logo.Size = New System.Drawing.Size(288, 288)
+        Me.Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.Logo.TabIndex = 0
+        Me.Logo.TabStop = False
+        '
+        'Bar
+        '
+        Me.Bar.Location = New System.Drawing.Point(-2, 332)
+        Me.Bar.Name = "Bar"
+        Me.Bar.Size = New System.Drawing.Size(312, 23)
+        Me.Bar.TabIndex = 4
+        '
+        'TextChargement
+        '
+        Me.TextChargement.Interval = 500
+        '
+        'Chargement
+        '
+        Me.Chargement.Interval = 80
+        '
+        'ChargementText
+        '
+        Me.ChargementText.AutoSize = True
+        Me.ChargementText.BackColor = System.Drawing.Color.Transparent
+        Me.ChargementText.Font = New System.Drawing.Font("Cartoonist", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ChargementText.ForeColor = System.Drawing.Color.White
+        Me.ChargementText.Location = New System.Drawing.Point(67, 308)
+        Me.ChargementText.Name = "ChargementText"
+        Me.ChargementText.Size = New System.Drawing.Size(167, 21)
+        Me.ChargementText.TabIndex = 1
+        Me.ChargementText.Text = "Chargement..."
+        '
+        'Copyright
+        '
+        Me.Copyright.AutoSize = True
+        Me.Copyright.BackColor = System.Drawing.Color.Transparent
+        Me.Copyright.Font = New System.Drawing.Font("Segoe UI Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Copyright.ForeColor = System.Drawing.Color.White
+        Me.Copyright.Location = New System.Drawing.Point(33, 358)
+        Me.Copyright.Name = "Copyright"
+        Me.Copyright.Size = New System.Drawing.Size(243, 17)
+        Me.Copyright.TabIndex = 5
+        Me.Copyright.Text = "©KyOS - Kycraft 2018 (Créé par Kyneas)"
+        '
+        'MAJtimer
+        '
+        Me.MAJtimer.Interval = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkRed
+        Me.ClientSize = New System.Drawing.Size(308, 384)
+        Me.Controls.Add(Me.Copyright)
+        Me.Controls.Add(Me.Bar)
+        Me.Controls.Add(Me.ChargementText)
+        Me.Controls.Add(Me.Logo)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "KyOS - Chargement..."
+        CType(Me.Logo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Logo As PictureBox
+    Friend WithEvents ChargementText As FlatLabel
+    Friend WithEvents Bar As ProgressBar
+    Friend WithEvents TextChargement As Timer
+    Friend WithEvents Chargement As Timer
+    Friend WithEvents Copyright As FlatLabel
+    Friend WithEvents MAJtimer As Timer
+End Class
---
+++ Chargement/KyOS Charger/Form1.vb
@@ -0,0 +1,73 @@
+﻿Imports System.Environment
+Imports System.Net
+Public Class Form1
+    Dim A As New WebClient
+    Dim MAJ As New WebClient
+    Dim Local As String = GetFolderPath(SpecialFolder.LocalApplicationData)
+    Dim VersionA As String = My.Settings.VersionA
+    Dim VersionMAJ As String = MAJ.DownloadString("http://kyos-update.livehost.fr/kyos_version/versionMAJ.txt")
+    Dim Erreur208 As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CheckUpdates()
+        MAJtimer.Start()
+    End Sub
+
+    Sub CheckUpdates()
+        If My.Computer.FileSystem.FileExists("C:\Program Files\KyOS\" + VersionA + "\KyOS.exe") Then
+        Else
+            My.Computer.FileSystem.CreateDirectory("C:\Program Files\KyOS\" + VersionA + "\")
+            My.Computer.Network.DownloadFile("http://kyos-update.livehost.fr/kyos_maj/" + VersionA + "/KyOS.exe", "C:\Program Files\KyOS\" + VersionA + "\KyOS.exe", False, 500)
+        End If
+        If My.Computer.FileSystem.FileExists("C:\Program Files\KyOS\" + VersionA + "\Fond\") Then
+        Else
+            My.Computer.FileSystem.CreateDirectory("C:\Program Files\KyOS\" + VersionA + "\Fond\")
+            My.Computer.Network.DownloadFile("http://kyos-update.livehost.fr/kyos_maj/" + VersionA + "/Fond/WallpaperDefault.png", "C:\Program Files\KyOS\" + VersionA + "\Fond\WallpaperDefault.png")
+        End If
+        If VersionA = VersionMAJ Then
+            MAJtimer.Stop()
+            Chargement.Start()
+            TextChargement.Start()
+        Else
+            Erreur208 = MsgBox("KyOS n'est pas à jour" & vbNewLine & "Faire la Mise à Jour ?", vbYesNo + MsgBoxStyle.Critical, "Erreur 208")
+        End If
+    End Sub
+
+    Private Sub MAJtimer_Tick(sender As Object, e As EventArgs) Handles MAJtimer.Tick
+        If Erreur208 = vbYes Then
+            My.Computer.FileSystem.CreateDirectory("C:\Program Files\KyOS\" + VersionMAJ + "\")
+            My.Computer.FileSystem.CreateDirectory("C:\Program Files\KyOS\" + VersionMAJ + "\Fond\")
+            My.Computer.Network.DownloadFile("http://kyos-update.livehost.fr/kyos_maj/" + VersionMAJ + "/Fond/WallpaperDefault.png", "C:\Program Files\KyOS\" + VersionMAJ + "\Fond\WallpaperDefault.png")
+            My.Computer.Network.DownloadFile("http://kyos-update.livehost.fr/kyos_maj/" + VersionMAJ + "/KyOS.exe", "C:\Program Files\KyOS\" + VersionMAJ + "\KyOS.exe", False, 500)
+            My.Settings.VersionA = VersionMAJ
+            My.Settings.Save()
+            MAJtimer.Stop()
+            Refresh()
+            Chargement.Start()
+            TextChargement.Start()
+        Else
+            MAJtimer.Stop()
+            TextChargement.Start()
+            Chargement.Start()
+        End If
+    End Sub
+
+    Private Sub TextChargement_Tick(sender As Object, e As EventArgs) Handles TextChargement.Tick
+        If ChargementText.Text = "Chargement..." Then
+            ChargementText.Text = "Chargement."
+        ElseIf ChargementText.Text = "Chargement." Then
+            ChargementText.Text = "Chargement.."
+        ElseIf ChargementText.Text = "Chargement.." Then
+            ChargementText.Text = "Chargement..."
+        End If
+    End Sub
+
+    Private Sub Chargement_Tick(sender As Object, e As EventArgs) Handles Chargement.Tick
+        Bar.Increment(1)
+        If Bar.Value = 100 Then
+            Shell("C:\Program Files\KyOS\" + VersionA + "\KyOS.exe")
+            Chargement.Stop()
+            Close()
+        End If
+    End Sub
+End Class
